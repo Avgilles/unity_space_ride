8cbeff10b4b78138d4a9b24d3eb6b5cbf4aa4bd5
Gilles Avraam <gillouavraam@gmail.com>
2021-10-15 08:26:17 +0200

    update turn arround

--- Assets/scripts/TurnArround.cs
+++ Assets/scripts/TurnArround.cs
@@ -6,9 +6,9 @@ public class TurnArround : MonoBehaviour
 {
     public float nbr = .5f;
     public Transform target;
-
     public float speed = 2.0f;
 
+    //public GameObject target;
     // Vector3 monVector = new Vector3(0,nbr,0);
     // Start is called before the first frame update
     void Start()
@@ -28,9 +28,13 @@ public class TurnArround : MonoBehaviour
         // transform.localScale = transform.localScale + new Vector3(0,5,0);
 
         // transform.LookAt(target);
-        transform.LookAt(new Vector3(0,0,0));
+        //transform.LookAt(new Vector3(0,0,0));
         // transform.Translate(1,0,0);
 
-        transform.RotateAround(target.transform.position, Vector3.up, 20* Time.deltaTime* speed);
+        //transform.RotateAround(target.transform.position, Vector3.up, 20* Time.deltaTime* speed);
+        foreach (Transform child in transform)
+        {
+            child.RotateAround(target.transform.position, Vector3.up, 20 * Time.deltaTime);
+        }
     }
 }
