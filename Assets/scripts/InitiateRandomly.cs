61c527178b9846ee2942166dae5a004b526873e6
Gilles Avraam <gillouavraam@gmail.com>
2021-10-17 03:05:26 +0200

    add random astéroides

--- Assets/scripts/InitiateRandomly.cs
+++ Assets/scripts/InitiateRandomly.cs
@@ -6,12 +6,22 @@ public class InitiateRandomly : MonoBehaviour
 {
     public Transform pos;
     public GameObject[] objectToInitiate;
+    public int nbrAsteroide = 20;
+    private Vector3 posVector3;
+    private int count;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        InstantiateObject();
+        Debug.Log(count);
+
+        for (count=0; count < nbrAsteroide; count++)
+        {
+            InstantiateObject(-10, 10);
+            Debug.Log(count);
+        }
+
 
     }
 
@@ -21,9 +31,19 @@ public class InitiateRandomly : MonoBehaviour
         
     }
 
-    private void InstantiateObject()
+    private void randomPos(int min,int max)
     {
+
+
+    }
+
+    private void InstantiateObject(int min, int max)
+    {
+        int randomPx = UnityEngine.Random.Range(min, max);
+        int randomPz = UnityEngine.Random.Range(min, max);
+        Vector3 result = new Vector3(randomPx,0, randomPz);
+
         int n = Random.Range(0, objectToInitiate.Length);
-        GameObject g = Instantiate(objectToInitiate[n], pos.position, objectToInitiate[n].transform.rotation);
+        GameObject g = Instantiate(objectToInitiate[n], result, objectToInitiate[n].transform.rotation);
     }
 }
