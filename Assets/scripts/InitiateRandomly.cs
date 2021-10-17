b666d409326538477c3d3cc80286bfee3c34ad3b
Gilles Avraam <gillouavraam@gmail.com>
2021-10-17 04:21:01 +0200

    random properly

--- Assets/scripts/InitiateRandomly.cs
+++ Assets/scripts/InitiateRandomly.cs
@@ -10,6 +10,10 @@ public class InitiateRandomly : MonoBehaviour
     private Vector3 posVector3;
     private int count;
 
+    public Vector2 beetween1;
+    public Vector2 beetween2;
+    public Vector2 beetween3;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +23,6 @@ public class InitiateRandomly : MonoBehaviour
         for (count=0; count < nbrAsteroide; count++)
         {
             InstantiateObject();
-            Debug.Log(count);
         }
 
 
@@ -31,17 +34,19 @@ public class InitiateRandomly : MonoBehaviour
         
     }
 
-    private Vector3 randomPos(int min,int max)
+    private Vector3 randomVector3(Vector2 axe_x, Vector2 axe_z, Vector2 axe_y = default(Vector2))
     {
-        int randomPx = UnityEngine.Random.Range(min, max);
-        int randomPz = UnityEngine.Random.Range(min, max);
-        Vector3 result = new Vector3(randomPx, 0, randomPz);
+        float randomPx = UnityEngine.Random.Range(axe_x.x, axe_x.y);
+        float randomPy = UnityEngine.Random.Range(axe_y.x, axe_y.y);
+        float randomPz = UnityEngine.Random.Range(axe_z.x, axe_z.y);
+
+        Vector3 result = new Vector3(randomPx, randomPy, randomPz);
         return result;
     }
 
     private void InstantiateObject()
     {
-        Vector3 Rpos = randomPos(-10, 10);
+        Vector3 Rpos = randomVector3(beetween1, beetween3);
 
         int n = Random.Range(0, objectToInitiate.Length);
         GameObject g = Instantiate(objectToInitiate[n], Rpos, objectToInitiate[n].transform.rotation);
